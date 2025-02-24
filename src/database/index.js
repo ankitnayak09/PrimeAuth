97527ed8d14c760d55e1657d64dad1c1cc90e53c
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2025-02-24 20:27:19 +0530

    Add JWT token generation functions for user authentication

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,27 @@
 import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
+const prisma = new PrismaClient();
 
-const prisma = new PrismaClient()
+const generateAccessToken = (user) => {
+	const { id, email, username, fullName } = user;
+	return jwt.sign(
+		{
+			id,
+			email,
+			username,
+			fullName,
+		},
+		process.env.ACCESS_TOKEN_SECRET,
+		{
+			expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+		}
+	);
+};
 
-export { prisma }
+const generateRefreshToken = (user) => {
+	const { username } = user;
+	return jwt.sign({ username }, process.env.REFRESH_TOKEN_SECRET, {
+		expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+	});
+};
+export { prisma, generateAccessToken, generateRefreshToken };
