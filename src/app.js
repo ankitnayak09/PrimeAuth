3dbc8c6e267058e3fa07089c39e4e8d2f991f2d4
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2025-02-24 21:01:09 +0530

    Add cookie-parser middleware for handling cookies in the application

--- src/app.js
+++ src/app.js
@@ -1,28 +1,30 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(express.json({limit: "16kb"}));
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 // Routes Import
-import  healthRouter from "./routes/health.route.js";
-import userRouter from "./routes/user.route.js"
+import healthRouter from "./routes/health.route.js";
+import userRouter from "./routes/user.route.js";
 
 // Routes Declaration
-app.use("/v1/health",healthRouter);
-app.use("/v1", userRouter)
+app.use("/v1/health", healthRouter);
+app.use("/v1", userRouter);
 
 // Error Handler
 app.use((err, req, res, next) => {
-  console.log(err);
-  return res.status(500).send("Something went wrong");
-})
+	console.log(err);
+	return res.status(500).send("Something went wrong");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
-})
+	console.log("Server is running on port", PORT);
+});
 
-export {app}
+export { app };
