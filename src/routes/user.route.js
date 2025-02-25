4ce99ad19da69862b4e9eddf40688c2dd1a86b32
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2025-02-25 19:38:12 +0530

    Add logout route to user routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,13 +3,10 @@ import { login, signup, verify } from "./../controllers/user.controller.js";
 import { Router } from "express";
 const router = Router();
 
-// TODO: Get User Details
 router.route("/verify").get(verify);
 
-// TODO: Register User
 router.route("/signup").post(signup);
 
-// TODO: Login User
 router.route("/login").post(login);
 
 // TODO: Change Password
@@ -17,5 +14,7 @@ router.route("/change-password", () => {});
 
 // TODO: Forgot Password
 router.route("/forgot-password", () => {});
+
+// TODO: Logout
 
 export default router;
